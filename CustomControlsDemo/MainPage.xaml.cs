a686089574fc46ff07305dc5174a1e2e7221e939
ilinkmobility <ramilink280217@gmail.com>
2018-03-02 19:00:32 +0530

    Initial project commit with RichPasswordBox control

---
+++ CustomControlsDemo/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CustomControlsDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        bool _validation;
+        public bool Validation
+        {
+            get { return _validation; }
+            set
+            {
+                _validation = value;
+                RaisePropertyChanged("Validation");
+            }
+        }
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var x = Validation;
+        }
+    }
+}
